48475713604248939574d65b365d5776cc288a76
Antonio Profico <antonio.profico@uniroma1.it>
2018-07-16 13:00:02 +0000

    version 1.0.2

---
+++ R/MAs_sets.R
@@ -0,0 +1,8 @@
+#' @title example dataset
+#' @description Landmark configurations of the manual alignments
+#' @name MAs_sets
+#' @docType data
+#' @author Antonio Profico, Alessio Veneziano, Marina Melchionna, Pasquale Raia
+#' @keywords Arothron
+#' @usage data(MAs_sets)
+NULL
---
+++ R/SM_set.R
@@ -0,0 +1,8 @@
+#' @title example dataset
+#' @description Landmark configuration associated to the starting model
+#' @name SM_set
+#' @docType data
+#' @author Antonio Profico, Alessio Veneziano, Marina Melchionna, Pasquale Raia
+#' @keywords Arothron
+#' @usage data(SM_set)
+NULL
